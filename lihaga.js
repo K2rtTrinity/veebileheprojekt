acf25f2bed499370c88054eeef290493a4178d1a
Trinity <karttrinity@gmail.com>
2023-12-03 14:29:58 +0200

    viimased muudatused

--- lihaga.js
+++ lihaga.js
@@ -1,13 +1,24 @@
-// defineerin funktsiooni toggleImage, mille argumendiks on retsepti ID
-function toggleImage(recipeId) {
-    // leiab html-ist elemendi, millel on retsepti ID ning salvestab selle muutujasse recipe
-    var recipe = document.getElementById(recipeId);
-    // otsib img elemnti ehk pildi elementi recipe elemendi alert, ehk põhimõtteliselt pildi linki ja salvestab muutujasse img
-    var img = recipe.querySelector('img');
-    // jällegi otsib recipe elemedi alt elementi button ja salvestab selle muutujasse button
-    var button = recipe.querySelector('button');
-    // muudab pildi läbipaistvuse nulliks ja üheks, olenevalt, kas on vaja pilti kuvada või mitte
-    img.style.opacity = (img.style.opacity === '0') ? '1' : '0';
-    // muudab nupu teksti olenevalt sellest, kas pilt on läbipaistev (nupu tekstiks 'Näita pilti') või mitte (nupu tekstiks 'Peida pilt')
-    button.textContent = (img.style.opacity === '0') ? 'Näita pilti' : 'Peida pilt';
+// OpenAI. (2023). ChatGPT [Large language model]. https://chat.openai.com
+// Kasutasin OpenAI-d JavaScripti koodi kirjutamisel, sest ma pole ise JavaScriptiga vare kokku puutunud.
+// Kuid nüüd mõistan mida ja milleks on vaja kasutada.
+
+// funktsioon võtab argumendiks retseptinumbri ja tema eesmärk on näidata/peita (vastavalt nupule klikkimisele) pilti.
+function pildiga(retseptiNumber) {
+    // Määrame muutujale pildiElement väärtuse
+    // document.getElementById on meetod, et otsida HTML dokumendist elementi, millel on ID
+    // näitaPilti on ID osa, millele lisatakse reteptiNumber, et saada täpne ID vastava pilti jaoks. 
+    // (HTML koodis oleme määranud piltide IDks "näitaPilti" + number(vastav järjekorra number))
+    var pildiElement = document.getElementById('näitaPilti' + retseptiNumber);
+
+    // Nüüd kontrollime, kas pilt on peidetud või mitte.
+    // Kui pilt on peidetud, siis funktsioon teeb selle nähtavaks.
+    // pildiElement.style.display annab juurdepääsu CSS-stiilile, kus reguleeritakse pildi nähtavust.
+    // 'none' – pilt on peidetud ja muudetakse nähtavaks
+    // 'block' – pilt on nähtaval ja muudetakse peidetuks
+    if (pildiElement.style.display === 'none') {
+        pildiElement.style.display = 'block';
+    // Kui pilt on nähtav, siis funktsioon peidab pildi
+    } else {
+        pildiElement.style.display = 'none';
+    }
 }
